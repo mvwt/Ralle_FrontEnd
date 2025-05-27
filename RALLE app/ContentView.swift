fd78f8660a651c44fdbefb97766e4793c563f045
Gavin Randolph <gavin.e.randolph@gmail.com>
2025-05-26 21:57:44 -0400

    Initial Commit

---
+++ RALLE app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RALLE app
+//
+//  Created by Gavin Randolph on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RALLE appTests/RALLE_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  RALLE_appTests.swift
+//  RALLE appTests
+//
+//  Created by Gavin Randolph on 5/26/25.
+//
+
+import Testing
+
+struct RALLE_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
